73a11827ed70b9f4e396b7e8426bf669f37ce840
dhq_boiler <dhq_boiler@live.jp>
2025-09-18 23:16:22 +0900

    Initial commit

---
+++ spec/ponkotsu/md/editor_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Ponkotsu::Md::Editor do
+  it "has a version number" do
+    expect(Ponkotsu::Md::Editor::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
