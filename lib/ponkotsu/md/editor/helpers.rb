3458e076ffc6a7ff07ceacb3051e21e78c630a13
dhq_boiler <dhq_boiler@live.jp>
2025-09-24 15:03:10 +0900

    Bugfix

--- lib/ponkotsu/md/editor/helpers.rb
+++ lib/ponkotsu/md/editor/helpers.rb
@@ -13,7 +13,7 @@ module PonkotsuMdEditor
     # @param form [ActionView::Helpers::FormBuilder] The form builder object.
     # @param attribute [Symbol, String] The attribute name to bind the editor to (e.g., :content).
     # @param content [String] The initial content to display in the editor.
-    # @param options [Hash] Editor configuration options (e.g., :lang, :preview, :tools, :placeholder).
+    # @param options [Hash] Editor configuration options (:bold :italic :strikethrough :heading1 :heading2 :heading3 :heading4 :heading5 :heading6 :unordered_list :ordered_list :check_list :blockquote :link :image :code :code_block :table :horizontal_rule).
     # @return [String] The rendered HTML for the Markdown editor.
     #
     # Example usage in a Rails view:
@@ -27,6 +27,7 @@ module PonkotsuMdEditor
       form = form[:form] if form.is_a?(Hash)
       attribute = attribute[:attribute] if attribute.is_a?(Hash)
       content = content[:attribute] if content.is_a?(Hash)
+      options = options[:options] if options.is_a?(Hash)
       render "ponkotsu_md_editor/editor", locals: { attribute: attribute, content: content, form: form, options: options }
     end
   end
